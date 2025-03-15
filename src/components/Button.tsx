4e93bd78f837b731244b9c92cc1924435563ffd8
Victor Matheus <victormatheus507@gmail.com>
2025-03-15 16:58:45 -0300

    add Button variant component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,25 +8,31 @@ import { ComponentProps } from 'react';
 type ButtonProps = ComponentProps<typeof GlueStackButton> & {
   title: string;
   isLoading?: boolean;
+  variant?: 'solid' | 'outline';
 };
 
-export function Button({ title, isLoading = false, ...rest }: ButtonProps) {
+export function Button({
+  title,
+  variant = 'solid',
+  isLoading = false,
+  ...rest
+}: ButtonProps) {
   return (
     <GlueStackButton
       w="$full"
       h="$14"
-      bg="$green700"
+      bg={variant === 'outline' ? 'transparent' : '$green700'}
       borderColor="$green500"
-      borderWidth="$0"
+      borderWidth={variant === 'outline' ? '$1' : '$0'}
       borderRadius="$sm"
-      $active-bg="$green500"
+      $active-bg={variant === 'outline' ? '$gray500' : '$green500'}
       disabled={isLoading}
       {...rest}
     >
       {isLoading ? (
-        <ButtonSpinner color='$white' />
+        <ButtonSpinner color="$white" />
       ) : (
-        <Text color="$white" fontFamily="$heading" fontSize="$sm">
+        <Text color={variant === 'outline' ? '$green500' : '$white'} fontFamily="$heading" fontSize="$sm">
           {title}
         </Text>
       )}
