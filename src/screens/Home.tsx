5b3fa4bb0c6ad29d2fe0a083c64fefb5bd3de053
Victor Matheus <victormatheus507@gmail.com>
2025-03-18 17:27:09 -0300

    navegando para lista de exercicio

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,6 +2,8 @@ import { ExerciseCard } from '@components/ExerciseCard';
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
 import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed';
+import { useNavigation } from '@react-navigation/native';
+import { AppNavigatorRoutesProps } from '@routes/app.routes';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 
@@ -22,6 +24,12 @@ export function Home() {
     'Peito',
     'Abdômen',
   ]);
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate('exercise');
+  }
 
   return (
     <VStack flex={1}>
@@ -56,7 +64,9 @@ export function Home() {
         <FlatList
           data={exercises}
           keyExtractor={(item) => item}
-          renderItem={() => <ExerciseCard />}
+          renderItem={() => (
+            <ExerciseCard onPress={handleOpenExerciseDetails} />
+          )}
           showsVerticalScrollIndicator={false}
           contentContainerStyle={{ paddingBottom: 20 }}
         />
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,14 @@
+import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
-import { Center, ScrollView, Text, VStack } from '@gluestack-ui/themed';
+import {
+  Center,
+  Heading,
+  ScrollView,
+  Text,
+  VStack,
+} from '@gluestack-ui/themed';
 import { TouchableOpacity } from 'react-native';
 
 export function Profile() {
@@ -31,7 +38,34 @@ export function Profile() {
 
           <Center w="$full" gap="$4">
             <Input placeholder="Nome" bg="$gray600" />
-            <Input value="victormatheus507@gmail.com" bg="$gray600" isReadOnly />
+            <Input
+              value="victormatheus507@gmail.com"
+              bg="$gray600"
+              isReadOnly
+            />
+          </Center>
+
+          <Heading
+            alignSelf="flex-start"
+            fontFamily="$heading"
+            color="$gray200"
+            fontSize="$md"
+            mt="$12"
+            mb="$2"
+          >
+            Alterar Senha
+          </Heading>
+
+          <Center w="$full" gap="$4">
+            <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry />
+            <Input placeholder="Nova senha" bg="$gray600" secureTextEntry />
+            <Input
+              placeholder="Confirme a nova senha"
+              bg="$gray600"
+              secureTextEntry
+            />
+
+            <Button title='Atualizar' />
           </Center>
         </Center>
       </ScrollView>
