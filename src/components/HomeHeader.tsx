85d93a70053ea20fc0e16d98c22c18e833c0101a
Victor Matheus <victormatheus507@gmail.com>
2025-03-18 15:29:49 -0300

    botao de logout

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,16 +1,17 @@
 import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed';
 import { UserPhoto } from './UserPhoto';
+import { LogOut } from 'lucide-react-native';
 
 export function HomeHeader() {
   return (
-    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap='$4'>
+    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
       <UserPhoto
         source={{ uri: 'https://github.com/V-Matheus.png' }}
         alt="Imagem do usuário"
-        w='$16'
-        h='$16'
+        w="$16"
+        h="$16"
       />
-      <VStack>
+      <VStack flex={1}>
         <Text color="$gray100" fontSize="$sm">
           olá
         </Text>
@@ -18,6 +19,8 @@ export function HomeHeader() {
           Home
         </Heading>
       </VStack>
+
+      <LogOut color="#ccc" size={24} />
     </HStack>
   );
 }
