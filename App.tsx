99261a8f3cadbdae6564d1fd3ae4be2672e6f785
Victor Matheus <victormatheus507@gmail.com>
2025-03-20 17:02:53 -0300

    unificando o create provider e o context

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import { Avatar, GluestackUIProvider } from '@gluestack-ui/themed';
 import { config } from './config/gluestack-ui.config';
 import { Loading } from '@components/Loading';
 import { Routes } from '@routes/index';
-import { AuthContext } from '@contexts/AuthContext';
+import { AuthContext, AuthContextProvider } from '@contexts/AuthContext';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,18 +19,9 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <AuthContext.Provider
-        value={{
-          user: {
-            id: '1',
-            name: 'Matheus',
-            email: 'victormatheus507@gmail.com',
-            avatar: 'matheus.png',
-          },
-        }}
-      >
+      <AuthContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </GluestackUIProvider>
   );
 }
--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,8 +1,27 @@
 import { UserDTO } from '@dtos/UserDto';
-import { createContext } from 'react';
+import { Children, createContext, ReactNode } from 'react';
 
 export type AuthContextDataProps = {
   user: UserDTO;
 };
 
-export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
+export const AuthContext = createContext<AuthContextDataProps>(
+  {} as AuthContextDataProps,
+);
+
+export function AuthContextProvider({ children }: { children: ReactNode }) {
+  return (
+    <AuthContext.Provider
+      value={{
+        user: {
+          id: '1',
+          name: 'Matheus',
+          email: 'victormatheus507@gmail.com',
+          avatar: 'matheus.png',
+        },
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
