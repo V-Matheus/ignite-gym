2d5c637e6c0e217cec9d59327d02b17cd0391996
Victor Matheus <victormatheus507@gmail.com>
2025-03-21 16:21:27 -0300

    persistindo usuario logado

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { UserDTO } from '@dtos/UserDTO';
 import { api } from '@services/api';
+import { storageUserSave } from '@storage/storageUser';
 import { Children, createContext, ReactNode, useState } from 'react';
 
 export type AuthContextDataProps = {
@@ -18,7 +19,10 @@ export function AuthContextProvider({ children }: { children: ReactNode }) {
     try {
       const { data } = await api.post('/sessions', { email, password });
 
-      if (data.user) setUser(data.user);
+      if (data.user) {
+        setUser(data.user);
+        storageUserSave(data.user);
+      }
     } catch (error) {
       throw error;
     }
