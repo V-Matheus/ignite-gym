7e8dd51577f3091aaf281beae53d7dd52062fbb6
Victor Matheus <victormatheus507@gmail.com>
2025-03-21 21:20:47 -0300

    exibindo detalhes do exercicio

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -15,14 +15,22 @@ import BodySvg from '@assets/body.svg';
 import SeriesSvg from '@assets/series.svg';
 import RepetitionsSvg from '@assets/repetitions.svg';
 import { Button } from '@components/Button';
+import { AppError } from '@utils/AppError';
+import { ToastMenssage } from '@components/ToastMenssage';
+import { useToast } from '@gluestack-ui/themed';
+import { api } from '@services/api';
+import { useEffect, useState } from 'react';
+import { ExerciseDTO } from '@dtos/ExerciseDTO';
 
 type RouteParamsProps = {
   exerciseId: string;
 };
 
 export function Exercise() {
-  const navigation = useNavigation<AppNavigatorRoutesProps>();
+  const [exercise, setExercise] = useState<ExerciseDTO>({} as ExerciseDTO);
 
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+  const toast = useToast();
   const route = useRoute();
 
   const { exerciseId } = route.params as RouteParamsProps;
@@ -30,6 +38,34 @@ export function Exercise() {
   function handleGoBack() {
     navigation.goBack();
   }
+
+  async function fetchExerciseDetails() {
+    try {
+      const response = await api(`/exercises/${exerciseId}`);
+      setExercise(response.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível carregar o exercício';
+
+      return toast.show({
+        placement: 'top',
+        render: ({ id }) => (
+          <ToastMenssage
+            id={id}
+            action="error"
+            title={title}
+            onClose={() => toast.close(id)}
+          />
+        ),
+      });
+    }
+  }
+
+  useEffect(() => {
+    fetchExerciseDetails();
+  }, [exerciseId]);
 
   return (
     <VStack flex={1}>
@@ -45,12 +81,12 @@ export function Exercise() {
             fontSize="$lg"
             flexShrink={1}
           >
-            Puxada Frontal
+            {exercise.name}
           </Heading>
           <HStack alignItems="center">
             <BodySvg />
             <Text color="$gray200" ml="$1" textTransform="capitalize">
-              Costas
+              {exercise.group}
             </Text>
           </HStack>
         </HStack>
@@ -61,17 +97,17 @@ export function Exercise() {
         contentContainerStyle={{ paddingBottom: 32 }}
       >
         <VStack p="$8">
-          <Image
-            source={{
-              uri: 'https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/costas-puxada-aberta-com-barra-no-pulley-1.gif',
-            }}
-            alt="Exercício"
-            mb="$3"
-            resizeMode="cover"
-            rounded="$lg"
-            w="$full"
-            h="$80"
-          />
+          <Box rounded="$lg" mb="$3" overflow="hidden">
+            <Image
+              source={{
+                uri: `${api.defaults.baseURL}/exercise/demo/${exercise.demo}`,
+              }}
+              alt="Exercício"
+              resizeMode="cover"
+              w="$full"
+              h="$80"
+            />
+          </Box>
 
           <Box bg="$gray600" rounded="$md" pb="$4" px="$4">
             <HStack
@@ -83,13 +119,13 @@ export function Exercise() {
               <HStack>
                 <SeriesSvg />
                 <Text color="$gray200" ml="$2">
-                  3 Séries
+                  {exercise.series} Séries
                 </Text>
               </HStack>
               <HStack>
                 <RepetitionsSvg />
                 <Text color="$gray200" ml="$2">
-                  12 repetições
+                  {exercise.repetitions} repetições
                 </Text>
               </HStack>
             </HStack>
