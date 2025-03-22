8a45b99ea8ae8ab8434fb33f7e96e662d5b97f8b
Victor Matheus <victormatheus507@gmail.com>
2025-03-21 21:59:35 -0300

    exibindo dados do historico

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -1,6 +1,11 @@
+import { HistoryDTO } from '@dtos/HistoryDTO';
 import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed';
 
-export function HistoryCard() {
+type HistoryCardProps = {
+  data: HistoryDTO;
+};
+
+export function HistoryCard({ data }: HistoryCardProps) {
   return (
     <HStack
       w="$full"
@@ -20,14 +25,14 @@ export function HistoryCard() {
           fontFamily="$heading"
           numberOfLines={1}
         >
-          Costas
+          {data.group}
         </Heading>
         <Text color="$gray100" fontSize="$lg" numberOfLines={1}>
-          Puxada Frontal
+          {data.name}
         </Text>
       </VStack>
       <Text color="$gray300" fontSize="$md">
-        08:56
+        {data.hour}
       </Text>
     </HStack>
   );
